7cf11b6c3e6f192c2e571ac6b91bc1a386ecdfef
Seung-hyun Kim <seunghyun4525@gmail.com>
2023-03-30 22:21:44 +0900

    feat: Static Site Generate 방식으로 자료구조 페이지를 생성하도록 구현

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
 import React, { useCallback, useState } from "react";
 
 import SearchInput from "@/components/molecules/SearchInput";
+import { SearchDataDS } from "@/types/types";
 
-export default function Sidebar() {
+type Props = {
+  searchDatas?: SearchDataDS[];
+};
+
+export default function Sidebar({ searchDatas = [] }: Props) {
   const [searchInput, setSearchInput] = useState("");
   const [searchWord, setSearchWord] = useState("");
   const isSearching = searchWord.length > 0;
@@ -29,7 +35,19 @@ export default function Sidebar() {
         onChange={handleSearchChange}
         onSubmit={handleSearchSubmit}
       />
-      {isSearching ? <div>검색결과 for {searchWord}</div> : <div>기본결과</div>}
+      {isSearching ? (
+        <div>검색결과 for {searchWord}</div>
+      ) : (
+        <nav>
+          <ul>
+            {searchDatas.map((searchData) => (
+              <li key={searchData.id}>
+                <Link href={searchData.link}>{searchData.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
     </div>
   );
 }
--- src/pages/data-structure/index.tsx
+++ src/pages/data-structure/index.tsx
@@ -1,3 +1,38 @@
-export default function DataStructure() {
-  return <h1 className="text-3xl font-bold underline">자료구조 페이지</h1>;
+import { GetStaticPropsResult } from "next/types";
+
+import Sidebar from "@/components/organisms/Sidebar";
+import { API_URL } from "@/constants/env";
+import { ONE_DAY } from "@/constants/time";
+import { DataStructureSearchInfo } from "@/types/types";
+
+type Props = {
+  search: DataStructureSearchInfo[];
+};
+
+export default function DataStructure({ search }: Props) {
+  return (
+    <div>
+      <Sidebar searchDatas={search} />
+      <div>자료구조 페이지</div>
+    </div>
+  );
+}
+
+export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
+  // Get data structure name list
+  const res = await fetch(`${API_URL}/data-structure/list`);
+  const dataStructureList = await res.json();
+
+  // Make object with { name, link }
+  const searchList: DataStructureSearchInfo[] = dataStructureList.map(
+    (name: string) => ({
+      name,
+      link: `/data-structure/${name.replaceAll(" ", "-")}`,
+    })
+  );
+
+  return {
+    props: { search: searchList },
+    revalidate: ONE_DAY, // Re-generate every day
+  };
 }

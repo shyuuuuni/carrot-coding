de7391eb1267f53f231796f15519716dd06ba483
Seung-hyun Kim <seunghyun4525@gmail.com>
2023-04-27 04:18:20 +0900

    styles: 페이지 레이아웃 크기 조절

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: Props) {
   return (
     <div className="flex h-full flex-col bg-gray-200 text-gray-900 dark:bg-gray-800 dark:text-gray-300">
       <Navbar />
-      <main className="flex flex-grow-0 flex-col items-center overflow-auto">
+      <main className="flex h-full flex-grow-0 flex-col items-center overflow-auto">
         {children}
       </main>
     </div>

775af418d4ec4b31d73fdbb741ef0d3e3e71663f
Seung-hyun Kim <seunghyun4525@gmail.com>
2023-04-28 22:38:36 +0900

    feat: 검색 필터링 기능 추가

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,7 +1,6 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
-import Link from "next/link";
-
+import SidebarMenu from "@/components/atoms/SidebarMenu";
 import SearchInput from "@/components/molecules/SearchInput";
 import { DataStructureSearchInfo } from "@/types/types";
 
@@ -12,7 +11,24 @@ type Props = {
 export default function Sidebar({ searchInfo = [] }: Props) {
   const [searchInput, setSearchInput] = useState("");
   const [searchWord, setSearchWord] = useState("");
-  const isSearching = searchWord.length > 0;
+
+  const sidebarMenus = useMemo(() => {
+    const info =
+      searchWord.length === 0
+        ? searchInfo
+        : searchInfo.filter(({ name }) => {
+            return name.en.includes(searchWord) || name.kr.includes(searchWord);
+          });
+
+    return info.map(({ name, link, description }) => (
+      <SidebarMenu
+        key={name.en}
+        name={name}
+        link={link}
+        description={description}
+      />
+    ));
+  }, [searchInfo, searchWord]);
 
   // Handle on changing input box
   const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> =
@@ -36,19 +52,7 @@ export default function Sidebar({ searchInfo = [] }: Props) {
         onChange={handleSearchChange}
         onSubmit={handleSearchSubmit}
       />
-      {isSearching ? (
-        <div>검색결과 for {searchWord}</div>
-      ) : (
-        <nav>
-          <ul>
-            {searchInfo.map((search) => (
-              <li key={search.name}>
-                <Link href={search.link}>{search.name}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      )}
+      {sidebarMenus}
     </div>
   );
 }
